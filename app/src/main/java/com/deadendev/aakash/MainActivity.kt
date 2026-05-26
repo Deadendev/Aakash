c20182a76489c96ec5aecc67481376812a34c799
DeadEnd Dev <punkcrypto58@gmail.com>
2026-05-26 14:29:38 +0400

    Add MainActivity.kt file

---
+++ app/src/main/java/com/deadendev/aakash/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.deadendev.aakash
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val textView = TextView(this)
+        textView.text = "Hello Aakash!"
+        setContentView(textView)
+    }
+}
